32f85537bf2250560b4af14ca5ea1c1b49ecd7f8
MoshiBin <MoshiBin@users.noreply.github.com>
2015-05-09 00:00:40 +0300

    Accept parameters via URL

--- js/graphino.js
+++ js/graphino.js
@@ -1,12 +1,25 @@
 
+
 $(function() {
   var graph = $("#graph");
-  var graphUrl = "https://www.hostedgraphite.com/32297e36/graphite/render/?width=640&height=480&target=test&title=test&lineMode=connected&lineWidth=4";
+  var graphUrl = "https://www.hostedgraphite.com/32297e36/graphite/render/";
   var fromTime = -10;
   var untilTime = 0;
   var bgcolor = null;
   var fgcolor = null;
   var tickSize = 3; // The amount of minutes to move using the arrows
+
+  var target = getParameterByName("target");
+  var width = getParameterByName("width");
+  var height = getParameterByName("height");
+  var title = getParameterByName("title");
+
+  function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#/]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+  }
 
   graph.load(function(e) {
     $(this).next(".toolbox").children(".loader").hide();
@@ -16,18 +29,18 @@ $(function() {
     graph.next(".toolbox").children(".loader").show();
     var fromTimeString = fromTime != 0 ? fromTime.toString() + "minutes" : "now";
     var untilTimeString = untilTime != 0 ? untilTime.toString() + "minutes" : "now";
-    url = graphUrl + "&from=" + fromTimeString + "&until=" + untilTimeString;
-    if (bgcolor != null) {
-      url = url + "&bgcolor=" + bgcolor;
-    }
-    if (fgcolor != null) {
-      url = url + "&fgcolor=" + fgcolor;
-    }
+    url = graphUrl + "?lineMode=connected&lineWidth=4&target=" + target + "&from=" + fromTimeString + "&until=" + untilTimeString;
+    console.log(url);
+    if (title != "") { url = url + "&title=" + title; }
+    if (width != "") { url = url + "&width=" + width; }
+    if (height != "") { url = url + "&height=" + height; }
+    if (bgcolor != null) { url = url + "&bgcolor=" + bgcolor; }
+    if (fgcolor != null) { url = url + "&fgcolor=" + fgcolor; }
 
     var now = new Date();
     url = url + "&cacheBuster=" + now.getTime();
 
-    graph.attr("src", graphUrl + "&from=" + fromTimeString + "&until=" + untilTimeString);
+    graph.attr("src", url);
   }
 
   $("#back").click(function(e) {
